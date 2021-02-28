630fe6e3519b6bcb32108174d8ee6d6264534ed6
diyorbekjumanov <diyorbekjumanov1120@gmail.com>
2021-02-28 06:22:49 +0000

    .

--- main.py
+++ main.py
@@ -4,18 +4,33 @@ import json
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/api')
-def capital():
-    data = open('capital_data.json').read() 
-    r = request.args
-    county = r.get('country', 'no')
+def home():
+    return "hi"
 
-    x = data.find(county)
-    if x != -1:
-        x1 = data.find(':', x) + 3
-        x2 = data.find('"', x1)
-        return data[x1:x2]
+@app.route('/api', methods=['GET','POST'])
+def capital():
+    data = open('capital_data.json').read()
+    if request.method == 'GET': 
+        r = request.args
+        county = r.get('country', 'no')
+        x = data.find(county)
+        if x != -1:
+            x1 = data.find(':', x) + 3
+            x2 = data.find('"', x1)
+            return data[x1:x2]
     
-    return 'no'
+        return 'not fount'
+    
+    if request.method == "POST":
+         
+        r = request.form
+        county = r.get('country', 'no')
+        x = data.find(county)
+        if x != -1:
+            x1 = data.find(':', x) + 3
+            x2 = data.find('"', x1)
+            return data[x1:x2]
+
+
 
 app.run(debug=True)
